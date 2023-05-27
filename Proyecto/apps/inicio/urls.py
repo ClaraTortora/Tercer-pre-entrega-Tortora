b35a76ef1880b2ecb3e6139e1aaee53c452254c7
ClaraTortora <claratortora@gmail.com>
2023-05-27 16:06:15 -0300

    Update de urls y views para el formulario

--- Proyecto/apps/inicio/urls.py
+++ Proyecto/apps/inicio/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('crear_profesor/', views.crear_profesor, name='crear_profesor'),
+   # path('crear_alumno/', views.crear_alumno, name='crear_alumno'),
+   # path('crear_materias/', views.crear_materias, name='crear_materias'),
 ]
--- Proyecto/apps/inicio/views.py
+++ Proyecto/apps/inicio/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .import forms
 
 # Create your views here.
 def index(request):
-    return render(request, 'inicio/index.html')
+    return redirect('inicio:index')
+
+def crear_profesor(request):
+    if request.method == 'POST':
+        form = forms.ProfesorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'inicio/index.html')
+    else:
+        form = forms.ProfesorForm()
+        context = {'form': form}
+    return render (request, 'inicio/crear_profesor.html', context)
