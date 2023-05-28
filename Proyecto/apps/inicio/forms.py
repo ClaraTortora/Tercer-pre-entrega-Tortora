c98a940fd9973b2f7195d5b253e187a9385f283c
ClaraTortora <claratortora@gmail.com>
2023-05-27 23:44:20 -0300

    Update de forms: agregando Materias y Alumno

--- Proyecto/apps/inicio/forms.py
+++ Proyecto/apps/inicio/forms.py
@@ -7,4 +7,16 @@ class ProfesorForm(forms.ModelForm):
         model = models.Profesor
         fields = '__all__'
         
-        
+class AlumnoForm(forms.ModelForm):
+    class Meta:
+        model = models.Alumno
+        fields = '__all__'
+
+
+class MateriasForm(forms.ModelForm):
+    class Meta:
+        model = models.Materias
+        fields = '__all__'
+
+
+
